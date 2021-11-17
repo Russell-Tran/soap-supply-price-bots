06989c0935a45776a010f554f1536a42ae109d96
Russell Tran <tranrl@stanford.edu>
2021-11-16 22:08:21 -0800

    Achieve functionality for essential_depot

--- selenium_scripts/README.md
+++ selenium_scripts/README.md
@@ -4,3 +4,5 @@ pipenv install
 brew install geckodriver
 pipenv run python main.py
 ```
+
+https://www.selenium.dev/selenium/docs/api/py/webdriver/selenium.webdriver.common.by.html
--- selenium_scripts/essential_depot.py
+++ selenium_scripts/essential_depot.py
@@ -9,6 +9,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
@@ -30,28 +31,70 @@ class EssentialDepot():
         d = self.driver
         p = profile
         
-        d.get(product_url)
-        d.find_element(By.ID, "js-add-to-cart").click()
-        d.find_element(By.ID, "js-mini-basket").click()
+        try:
+            d.get(product_url)
+            d.find_element(By.ID, "js-add-to-cart").click()
+            d.find_element(By.ID, "js-mini-basket").click()
 
-        time.sleep(3)
-        element = d.find_element(By.XPATH, "/html/body/div[3]/header/div[2]/div[3]/div[1]/div[3]/a")
-        d.execute_script("arguments[0].click();", element)
+            time.sleep(3)
+            element = d.find_element(By.XPATH, "/html/body/div[3]/header/div[2]/div[3]/div[1]/div[3]/a")
+            d.execute_script("arguments[0].click();", element)
 
-        time.sleep(3)
-        d.find_element(By.LINK_TEXT, "Continue as a Guest").click()
-        
-        time.sleep(3)
-        d.find_element(By.ID, "js-billing-to-show").click()
-        d.find_element(By.ID, "l-ShipFirstName").send_keys(p['shipping_address']['first_name'])
-        d.find_element(By.ID, "l-ShipLastName").send_keys(p['shipping_address']['last_name'])
-        d.find_element(By.ID, "l-ShipEmail").send_keys(p['shipping_address']['email'])
-        d.find_element(By.ID, "l-ShipPhone").send_keys(p['shipping_address']['phone'])
-        d.find_element(By.ID, "l-ShipFax").send_keys(p['shipping_address']['fax'])
+            time.sleep(3)
+            d.find_element(By.LINK_TEXT, "Continue as a Guest").click()
+            
+            time.sleep(3)
+            d.find_element(By.ID, "js-billing-to-show").click()
 
-        print("Done")
-        time.sleep(5)
-        self.stop()
+            # Enter shipping info
+            d.find_element(By.ID, "l-ShipFirstName").send_keys(p['shipping_address']['first_name'])
+            d.find_element(By.ID, "l-ShipLastName").send_keys(p['shipping_address']['last_name'])
+            d.find_element(By.ID, "l-ShipEmail").send_keys(p['shipping_address']['email'])
+            d.find_element(By.ID, "l-ShipPhone").send_keys(p['shipping_address']['phone'])
+            d.find_element(By.ID, "l-ShipFax").send_keys(p['shipping_address']['fax'])
+            d.find_element(By.ID, "l-ShipCompany").send_keys(p['shipping_address']['company'])
+            d.find_element(By.ID, "l-ShipAddress1").send_keys(p['shipping_address']['address'])
+            d.find_element(By.ID, "l-ShipAddress2").send_keys(p['shipping_address']['address_2'])
+            d.find_element(By.ID, "l-ShipCity").send_keys(p['shipping_address']['city'])
+            element = d.find_element(By.ID, "ShipStateSelect")
+            Select(element).select_by_visible_text(p['shipping_address']['state'])
+            d.find_element(By.ID, "l-ShipZip").send_keys(p['shipping_address']['zipcode'])
+            element = d.find_element(By.ID, "ShipCountry")
+            Select(element).select_by_visible_text(p['shipping_address']['country'])
+
+            # Enter billing info
+            d.find_element(By.ID, "l-BillFirstName").send_keys(p['billing_address']['first_name'])
+            d.find_element(By.ID, "l-BillLastName").send_keys(p['billing_address']['last_name'])
+            d.find_element(By.ID, "l-BillEmail").send_keys(p['billing_address']['email'])
+            d.find_element(By.ID, "l-BillPhone").send_keys(p['billing_address']['phone'])
+            d.find_element(By.ID, "l-BillFax").send_keys(p['billing_address']['fax'])
+            d.find_element(By.ID, "l-BillCompany").send_keys(p['billing_address']['company'])
+            d.find_element(By.ID, "l-BillAddress1").send_keys(p['billing_address']['address'])
+            d.find_element(By.ID, "l-BillAddress2").send_keys(p['billing_address']['address_2'])
+            d.find_element(By.ID, "l-BillCity").send_keys(p['billing_address']['city'])
+            element = d.find_element(By.ID, "BillStateSelect")
+            Select(element).select_by_visible_text(p['billing_address']['state'])
+            d.find_element(By.ID, "l-BillZip").send_keys(p['billing_address']['zipcode'])
+            element = d.find_element(By.ID, "BillCountry")
+            Select(element).select_by_visible_text(p['billing_address']['country'])
+
+            # Continue to shipping
+            d.find_element(By.CSS_SELECTOR, "input.bg-red").click()
+            subtotal = d.find_element(By.CSS_SELECTOR, "strong.column:nth-child(2)").text
+            # Next page
+            d.find_element(By.CSS_SELECTOR, "input.button:nth-child(2)").click()
+            shipping = d.find_element(By.CSS_SELECTOR, "p.whole:nth-child(2) > span:nth-child(2)").text
+            total = d.find_element(By.CSS_SELECTOR, "p.column:nth-child(4) > strong:nth-child(2)").text
+            
+            print("subtotal: ", subtotal)
+            print("shipping: ", shipping)
+            print("total: ", total)
+            print("Done")
+            time.sleep(5)
+            self.stop()
+        except Exception as e:
+            self.stop()
+            raise e
 
         # self.driver.get("https://www.brambleberry.com/shop-by-product/ingredients/waxes/premium-yellow-beeswax/V001192.html")
         # self.driver.set_window_size(1800, 850)
