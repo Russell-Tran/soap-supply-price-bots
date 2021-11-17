d1410d4d73e85b57b56545cc3a0a7698ffb4613f
Russell Tran <tranrl@stanford.edu>
2021-11-16 22:21:28 -0800

    Cosmetic

--- selenium_scripts/essential_depot.py
+++ selenium_scripts/essential_depot.py
@@ -96,63 +96,6 @@ class EssentialDepot():
             self.stop()
             raise e
 
-        # self.driver.get("https://www.brambleberry.com/shop-by-product/ingredients/waxes/premium-yellow-beeswax/V001192.html")
-        # self.driver.set_window_size(1800, 850)
-        
-        # timeout = 5
-        # try:
-        #     # https://stackoverflow.com/a/37303115/14775744
-        #     element_present = EC.presence_of_element_located((By.ID, "CybotCookiebotDialogBodyUnderlay"))
-        #     WebDriverWait(self.driver, timeout).until(element_present)
-        # except TimeoutException:
-        #     print("Timed out waiting for page to load")
-
-        # time.sleep(3)
-        # # Cookie
-        # self.driver.find_element(By.ID, "CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll").click()
-        # time.sleep(3)
-
-        # # Product
-        # self.driver.find_element(By.LINK_TEXT, "5 lbs").click()
-        # time.sleep(1)
-
-        # self.driver.find_element(By.ID, "add-to-cart").click()
-
-        # # Cart
-        # self.driver.get("https://www.brambleberry.com/shoppingbag")
-        # time.sleep(2)
-
-        # #self.driver.execute_script("window.scrollTo(0,7)")
-        # #self.driver.execute_script("window.scrollTo(0,69)")
-        # #element = self.driver.find_element(By.CLASS_NAME, "estimate-shipping")
-        # #actions = ActionChains(self.driver)
-        # #actions.move_to_element(element).click().perform()
-
-        # # https://stackoverflow.com/a/63157469/14775744
-        # # https://stackoverflow.com/questions/41857614/how-to-find-xpath-of-an-element-in-firefox-inspector
-        # xpath = "/html/body/div[1]/div[3]/div/div[2]/div[2]/form/div/div[1]/table/tbody/tr[5]/td[1]/table/tbody/tr[1]/td/a"
-        # element = self.driver.find_element(By.XPATH, xpath)
-        # self.driver.execute_script("arguments[0].click();", element)
-
-        # self.driver.find_element(By.ID, "dwfrm_shippingestimator_shippingestimate_zipcode").click()
-        # self.driver.find_element(By.ID, "dwfrm_shippingestimator_shippingestimate_zipcode").send_keys("92673")
-        # time.sleep(1)
-        # self.driver.find_element(By.NAME, "dwfrm_cart_estimate").click()
-        # time.sleep(2)
-
-        # shipping = self.driver.find_element(By.CSS_SELECTOR, ".order-shipping > .align-right").text
-        # total = self.driver.find_element(By.CSS_SELECTOR, ".order-value").text
-
-        # print("Shipping ", shipping)
-        # print("Total ", total)
-
-        # """
-        # self.driver.execute_script("window.scrollTo(0,26)")
-        
-    
-        # self.stop()
-        # """
-
 if __name__ == "__main__":
     with open('profile.json') as file:
         profile = json.load(file)
