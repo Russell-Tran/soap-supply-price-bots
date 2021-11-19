8676c073f2cbc31c6e0bfda9f055f419a5b34755
Russell Tran <tranrl@stanford.edu>
2021-11-19 08:47:53 -0800

    Finish first implementation of bulk_apothecary

--- selenium_scripts/bulk_apothecary.py
+++ selenium_scripts/bulk_apothecary.py
@@ -46,22 +46,46 @@ class BulkApothecary():
             d.get("https://www.bulkapothecary.com/checkout")
             time.sleep(0.5)
             d.find_element(By.ID, "email").send_keys(p['shipping_address']['email'])
+            time.sleep(5) # failed on 1 second
             d.find_element(By.ID, "checkout-customer-continue").click()
-            time.sleep(0.5)
+            time.sleep(5)
 
             d.find_element(By.ID, "firstNameInput").send_keys(p['shipping_address']['first_name'])
-            d.find_element(By.ID, "lastNameInput").send_keys(p['shipping_addresss']['last_name'])
+            d.find_element(By.ID, "lastNameInput").send_keys(p['shipping_address']['last_name'])
             element = d.find_element(By.ID, "countryCodeInput")
             Select(element).select_by_visible_text(p['billing_address']['country'])
-            d.find_element(By.ID, "postCodeInput").send_keys(p['shipping_address']['zipcode'])
-            d.find_element(By.ID, "cityInput").send_keys(p['shipping_address']['city'])
+
+            # https://stackoverflow.com/a/44184600/14775744
+            elem = d.find_element(By.ID, "postCodeInput")
+            elem.send_keys(p['shipping_address']['zipcode'])
+            elem.send_keys(Keys.ARROW_DOWN)
+            # wait for the first dropdown option to appear and open it
+            # first_option = WebDriverWait(d, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#ui-id-307")))
+            # first_option.send_keys(Keys.RETURN)
+            time.sleep(5)
+            elem.send_keys(Keys.RETURN)
+            
+            element = d.find_element(By.ID, "provinceCodeInput")
+            Select(element).select_by_visible_text(p['shipping_address']['state'])
+            # d.find_element(By.ID, "cityInput").send_keys(p['shipping_address']['city']) dont need to provide city
             d.find_element(By.ID, "addressLine1Input").send_keys(p['shipping_address']['address'])
-            d.find_element(By.ID, "addressLine2Input").send_keys(p['shipping_address']['address_2'])
+            # d.find_element(By.ID, "addressLine2Input").send_keys(p['shipping_address']['address_2']) TODO: have to deal with popup that suspects bad suite #
+            d.find_element(By.ID, "companyInput").send_keys(p['shipping_address']['company'])
+            d.find_element(By.ID, "phoneInput").send_keys(p['shipping_address']['phone'])
+            time.sleep(7)
 
+            d.find_element(By.CSS_SELECTOR, "li.form-checklist-item:nth-child(1) > div:nth-child(1) > div:nth-child(1) > label:nth-child(2)").click()
+            time.sleep(5)
 
+            subtotal = d.find_element(By.CSS_SELECTOR, ".cart-priceItem--subtotal > span:nth-child(2) > span:nth-child(1)").text
+            shipping = d.find_element(By.CSS_SELECTOR, "section.cart-section:nth-child(3) > div:nth-child(2) > div:nth-child(1) > span:nth-child(2) > span:nth-child(1)").text
+            tax = d.find_element(By.CSS_SELECTOR, "section.cart-section:nth-child(3) > div:nth-child(3) > div:nth-child(1) > span:nth-child(2) > span:nth-child(1)").text
+            total = d.find_element(By.CSS_SELECTOR, ".cart-priceItem--total > span:nth-child(2) > span:nth-child(1)").text
 
-
-
+            print("Subtotal: ", subtotal)
+            print("Shipping: ", shipping)
+            print("Tax: ", tax)
+            print("Total: ", total)
 
             print("Done")
             time.sleep(5)
