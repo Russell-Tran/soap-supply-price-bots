fd53d310b02e187d3089e6cead82c2ab566228a3
Russell Tran <tranrl@stanford.edu>
2021-12-05 00:36:46 -0800

    Complete chemistry store

--- selenium_scripts/chemistry_store.py
+++ selenium_scripts/chemistry_store.py
@@ -4,6 +4,7 @@ Zap Sourcing 2021
 import pytest
 import time
 import json
+import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
@@ -33,6 +34,7 @@ class ChemistryStore():
         
         try:
             # Required minimum $10 so had to go with 2lb option
+            # TODO: profile should have boolean commercial address
             d.get(product_url)
             time.sleep(3)
             d.find_element(By.CSS_SELECTOR, "div.mt10:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(3)").click()
@@ -40,17 +42,52 @@ class ChemistryStore():
             time.sleep(5)
             d.get(shopping_cart_url)
             d.find_element(By.ID, "return_login").click()
+            element = d.find_element(By.CSS_SELECTOR, ".customer-account > ol:nth-child(2) > li:nth-child(1) > select:nth-child(2)")
+            Select(element).select_by_visible_text("Corporation")
+            d.find_element(By.ID, "email").send_keys(p['shipping_address']['email'])
+            time.sleep(3)
+            element = d.find_element(By.CSS_SELECTOR, ".billing-address > ol:nth-child(2) > li:nth-child(1) > input:nth-child(2)")
+            element.send_keys(p['shipping_address']['first_name'])
+            d.find_element(By.CSS_SELECTOR, ".billing-address > ol:nth-child(2) > li:nth-child(2) > input:nth-child(2)").send_keys(p['shipping_address']['last_name'])
+            d.find_element(By.CSS_SELECTOR, ".billing-address > ol:nth-child(2) > li:nth-child(3) > input:nth-child(2)").send_keys(p['shipping_address']['company'])
+            d.find_element(By.CSS_SELECTOR, ".billing-address > ol:nth-child(2) > li:nth-child(4) > input:nth-child(2)").send_keys(p['shipping_address']['address'])
+            d.find_element(By.CSS_SELECTOR, ".billing-address > ol:nth-child(2) > li:nth-child(6) > input:nth-child(2)").send_keys(p['shipping_address']['city'])
+            element = d.find_element(By.ID, "country")
+            Select(element).select_by_visible_text(p['shipping_address']['country'])
+            element = d.find_element(By.CSS_SELECTOR, "#country_fields > li:nth-child(1) > select:nth-child(2)")
+            Select(element).select_by_visible_text(p['shipping_address']['state'])
+            d.find_element(By.CSS_SELECTOR, "#country_fields > li:nth-child(2) > input:nth-child(2)").send_keys(p['shipping_address']['zipcode'])
+
+            # Is this a commercial address? Clicking by default rn
+            # (Notice: move_to_element required to be able to click the checkbox)
+            element = d.find_element(By.XPATH, "/html/body/div[6]/div[2]/div/div/form/div[1]/div[5]/fieldset/ol/li[8]/div/label/input")
+            actions = ActionChains(d)
+            actions.move_to_element(element).perform()
+            element.click()
+
+            d.find_element(By.CSS_SELECTOR, ".billing-address > ol:nth-child(2) > li:nth-child(10) > input:nth-child(2)").send_keys(p['shipping_address']['phone'])
+            d.find_element(By.CSS_SELECTOR, ".billing-address > ol:nth-child(2) > li:nth-child(11) > input:nth-child(2)").send_keys(p['shipping_address']['phone'])
+            d.find_element(By.ID, "calculate_button").click()
+
+            time.sleep(7)
+            d.find_element(By.CSS_SELECTOR, "#shipping_data > ol:nth-child(6) > li:nth-child(4) > input:nth-child(1)").click()
+
+            subtotal = d.find_element(By.CSS_SELECTOR, ".subtotal > span:nth-child(1)").text
+            subtotal = re.search('\$[0-9.]+', subtotal).group()   # Returns '$249.99'
+            shipping = d.find_element(By.ID, "ShippingCost").text
+            tax = d.find_element(By.ID, "taxid").text
+            total = d.find_element(By.ID, "Total").text
 
 
-            # print("Subtotal: ", subtotal)
-            # print("Shipping: ", shipping)
-            # print("Total: ", total)
-            # print("Done")
-            # time.sleep(5)
-            # self.stop()
+            print("Subtotal: ", subtotal)
+            print("Shipping: ", shipping)
+            print("Total: ", total)
+            print("Done")
+            time.sleep(5)
+            self.stop()
 
         except Exception as e:
-            self.stop()
+            #self.stop()
             raise e
 
 if __name__ == "__main__":
