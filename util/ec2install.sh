2f5c00531c6e97058b8f1212eb92f2407f005922
Ubuntu <ubuntu@ip-172-31-33-204.us-east-2.compute.internal>
2021-12-19 03:20:33 +0000

    Get script to successfully install geckodriver

--- util/ec2install.sh
+++ util/ec2install.sh
@@ -1,10 +1,15 @@
+# Run with sudo
+
 add-apt-repository ppa:deadsnakes/ppa   
 apt-get update 
 apt install python3.7
 apt install pipenv
 
 # https://askubuntu.com/a/871077
-wget https://github.com/mozilla/geckodriver/releases/download/v0.30.0/geckodriver-v0.30.0-linux64.tar.gz
-tar -xvzf geckodriver*
-chmod +x geckodriver
-#export PATH=$PATH:/path-to-extracted-file/.
+wget https://github.com/mozilla/geckodriver/releases/download/v0.30.0/geckodriver-v0.30.0-linux64.tar.gz -O /home/ubuntu/geckodriver-v0.30.0-linux64.tar.gz
+tar -xvzf /home/ubuntu/geckodriver* -C /home/ubuntu/
+chmod +x /home/ubuntu/geckodriver
+
+# Make sure to add the following line to ~/.bashrc
+# export PATH=$PATH:/home/ubuntu/
+# Don't forget to `source .bashrc` if you want to have this modification take effect without logging out and back in to the instance.
