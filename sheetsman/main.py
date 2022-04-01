23e66047754d02af6ad7a3d2b60271523e553d26
Russell Tran <tranrl@stanford.edu>
2022-04-01 10:03:57 -0700

    Back to fantastic-engine-natalie

--- sheetsman/main.py
+++ sheetsman/main.py
@@ -3,7 +3,9 @@ import selbots
 
 SPREADSHEET_DOCUMENT_NAME_HERE = 'cs_191_bot_scrape'
 
-if __name__ == "__main__":
-    sheetsman.fulfill(SPREADSHEET_DOCUMENT_NAME_HERE, 0)
-    for i in range(2, 17):
-        sheetsman.fulfill(SPREADSHEET_DOCUMENT_NAME_HERE, i)
+sheetsman.fulfill("fantastic-engine-natalie")
+
+# if __name__ == "__main__":
+#     sheetsman.fulfill(SPREADSHEET_DOCUMENT_NAME_HERE, 0)
+#     for i in range(2, 17):
+#         sheetsman.fulfill(SPREADSHEET_DOCUMENT_NAME_HERE, i)
